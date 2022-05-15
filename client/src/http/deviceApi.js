453564371b87c3e034752ca8689bf754be7977d5
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-05-15 21:30:32 +0300

    added Select and added functionality

--- client/src/http/deviceApi.js
+++ client/src/http/deviceApi.js
@@ -42,9 +42,16 @@ export const createDevice = async (device) => {
   return data;
 };
 
-export const fetchDevices = async (typeId, brandId, value, page, limit = 5) => {
+export const fetchDevices = async (
+  typeId,
+  brandId,
+  value,
+  filter,
+  page,
+  limit = 5
+) => {
   const { data } = await $host.get("api/device", {
-    params: { typeId, brandId, value, page, limit },
+    params: { typeId, brandId, value, filter, page, limit },
   });
   return data;
 };
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -12,13 +12,16 @@ import { fetchBrands, fetchTypes, fetchDevices } from "../http/deviceApi";
 const Shop = observer(() => {
   const { device } = useContext(Context);
   const [inputValue, setInputValue] = useState("");
+  const [filter, setFilter] = useState('"createdAt" DESC');
   useEffect(() => {
     fetchTypes().then((data) => device.setTypes(data));
     fetchBrands().then((data) => device.setBrands(data));
-    fetchDevices(null, null, inputValue, 1, device.limit).then((data) => {
-      device.setDevices(data.rows);
-      device.setTotalCount(data.count);
-    });
+    fetchDevices(null, null, inputValue, filter, 1, device.limit).then(
+      (data) => {
+        device.setDevices(data.rows);
+        device.setTotalCount(data.count);
+      }
+    );
   }, []);
 
   useEffect(() => {
@@ -26,13 +29,20 @@ const Shop = observer(() => {
       device.selectedType.id,
       device.selectedBrand.id,
       inputValue,
+      filter,
       device.page,
       device.limit
     ).then((data) => {
       device.setDevices(data.rows);
       device.setTotalCount(data.count);
     });
-  }, [device.page, device.selectedType, device.selectedBrand, inputValue]);
+  }, [
+    device.page,
+    device.selectedType,
+    device.selectedBrand,
+    inputValue,
+    filter,
+  ]);
 
   const changeInput = (e) => {
     setInputValue(e.target.value);
@@ -55,6 +65,12 @@ const Shop = observer(() => {
         <Col md={9}>
           <BrandBar />
           <input onChange={debouncedChangeHandler} />
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="price ASC">ASC</option>
+            <option value="price DESC">DESC</option>
+            <option value='"createdAt" ASC'>OLD</option>
+            <option value='"createdAt" DESC'>NEW</option>
+          </select>
 
           <DeviceList />
           <Pages />
